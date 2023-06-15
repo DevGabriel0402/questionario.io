c3606df498aaf811ea5b72b7889c5aa144879034
DevGabriel0402 <136494625+DevGabriel0402@users.noreply.github.com>
2023-06-15 07:59:05 -0300

    app.js

--- app.js
+++ app.js
@@ -70,6 +70,20 @@ let pergunta2 = document.getElementById("pergunta2");
 let pergunta3 = document.getElementById("pergunta3");
 let pergunta4 = document.getElementById("pergunta4");
 
+// Teste
+
+function proximo() {
+  // Obtém o valor do campo de texto
+  let textAreaValue = document.getElementById("CAT_Custom_1").value;
+
+  // Verifica se o campo de texto está vazio ou não
+  if (textArea1.trim() === "" || textArea2.trim() === "" || textArea3.trim() === "" || textArea4.trim() === "" ) {
+    // Se estiver vazio, exibe uma mensagem de erro ou faz alguma ação desejada
+    alert("O campo é obrigatório. Por favor, preencha-o.");
+    return; // Interrompe o envio do formulário
+  }
+  // 
+
 // Event listener para o botão "Próximo"
 $(".next").click(function () {
   if (animating) return false;
